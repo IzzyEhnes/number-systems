83e728f75c948ed140abdb12d73520a267a75079
IzzyEhnes <izzyehnes@gmail.com>
2020-07-24 16:01:10 -0700

    Feature: Add sevensComplement and eightsComplement methods

--- Octal.java
+++ Octal.java
@@ -134,6 +134,57 @@ public class Octal
 
 
 
+    public Octal sevensComplement()
+    {
+        int decimalPosition = this.getDecimalPosition();
+
+        StringBuilder sb = new StringBuilder();
+
+        // Append number of 7s that are to the right of the decimal point in final number
+        sb.append("7".repeat(decimalPosition));
+        // Append number of 7s that are to the left of the decimal point in final number
+        sb.append("7".repeat(this.octal.length() - 1 - decimalPosition));
+
+        String sevens = sb.reverse().toString();
+
+        // Reset sb
+        sb.setLength(0);
+
+        sb.append(this.octal);
+        sb.reverse().deleteCharAt(decimalPosition);
+
+        String octalString = sb.reverse().toString();
+
+        // Reset sb
+        sb.setLength(0);
+
+        // Find difference of the digits of 'sevens' and 'octalString'
+        for (int i = 0; i < octalString.length(); i++)
+        {
+            sb.append((sevens.charAt(i) - '0') - (octalString.charAt(i) - '0'));
+        }
+
+        sb.reverse().insert(decimalPosition, '.');
+
+        return new Octal(sb.reverse().toString());
+    }
+
+
+
+    public Octal eightsComplement()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        Octal sevensComplement = this.sevensComplement();
+        Octal one = new Octal("1.0");
+
+        sb.append(sevensComplement.addOctal(one));
+
+        return new Octal(sb.toString());
+    }
+
+
+
     public int getDecimalPosition()
     {
         StringBuilder sb = new StringBuilder();
@@ -142,6 +193,7 @@ public class Octal
 
         String a = sb.reverse().toString();
 
+        // Find decimal position with respect to rightmost digit
         int decimalPosition = 0;
         for (int i = 0; i < a.length(); i++)
         {
